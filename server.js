a9553b4328cbd66e6f3394469f9b2d153b0e4cae
Marcus Baptiste <marbap45@gmail.com>
2018-05-07 12:52:54 -0400

    Update port and host variables to use on different servers

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var http = require('http');
+var port = 8080;
+var host = '127.0.0.1';
+
 var server = http.createServer(function(req, res){
     res.writeHead(200, { 'Content-Type' : 'text/html'});
     res.end('place timestamp here');
 });
 
-server.listen(process.ENV.port, process.ENV.host);
+server.listen(port, host);
 
-console.log('Server listening on port ' + process.ENV.port);
+console.log('Server listening on port ' + port); 
